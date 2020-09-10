961febdf57c849e885deead4a68d564966f4432d
vahid.nia <vahid.nia@piworks.net>
2020-09-10 13:58:36 +0300

    Enm3gppToCsvKVPCore Project added

---
+++ Enm3gppToCsvKVPNetCore/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Enm3gppToCsvKVPNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: enm3gpp2kvp <source.xml> <target.csv>");
+                return;
+            }
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Enm3GPPToCsvKVPConverter.Convert(args[0], args[1], args[2], null);
+            sw.Stop();
+            Console.WriteLine($"Completed in {sw.Elapsed.TotalSeconds} seconds");
+        }
+    }
+}
