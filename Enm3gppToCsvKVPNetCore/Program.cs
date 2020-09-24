f3d72a79547e066f96eae5fd4e5f11da637db98d
vahid.nia <vahid.nia@piworks.net>
2020-09-24 10:56:52 +0300

    update dot net core version

--- Enm3gppToCsvKVPNetCore/Program.cs
+++ Enm3gppToCsvKVPNetCore/Program.cs
@@ -8,46 +8,23 @@ using System.Xml.Linq;
 
 namespace Enm3gppToCsvKVPNetCore
 {
+
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
 
             if (args.Length != 4)
             {
-                Console.WriteLine("Usage: enm3gpp2kvp <source.xml> <target.csv>");
+                Console.WriteLine("Usage: enm3gpp2kvp <source> <target.csv> <ossid> <model>");
                 return;
             }
             Stopwatch sw = new Stopwatch();
             sw.Start();
-
-
-            Enm3GPPToCsvKVPConverter.Convert(args[0], args[1], args[2], ProcessModelTxt(args[3]));
+            Enm3GPPToCsvKVPConverter.Convert(args[0], args[1], args[2], args[3]);
             sw.Stop();
             Console.WriteLine($"Completed in {sw.Elapsed.TotalSeconds} seconds");
         }
-
-        private static Dictionary<string, bool> ProcessModelTxt(string v)
-        {
-            Dictionary<string, Boolean> attDataType = new Dictionary<string, Boolean>();
-            foreach (var item in Directory.GetFiles(v, "*MODEL*.xml"))
-                ProcessModel(attDataType, item);
-
-            foreach (var item in Directory.GetFiles(v, "*com*.xml"))
-                ProcessModel(attDataType, item);
-            return attDataType;
-        }
-
-        private static void ProcessModel(Dictionary<string, bool> attDataType, string item)
-        {
-            Match match = Regex.Match(File.ReadAllText(item), @"<attribute\sname=\""(?<attname>\w+)\"">.+?<dataType>(?<dataType>.+?)</dataType>", RegexOptions.Singleline);
-            while (match.Success)
-            {
-                var isArray = match.Groups["dataType"].Value.Contains("<sequence>");
-                if (!attDataType.ContainsKey(match.Groups["attname"].Value))
-                    attDataType.Add(match.Groups["attname"].Value, isArray);
-                match = match.NextMatch();
-            }
-        }
     }
+
 }
